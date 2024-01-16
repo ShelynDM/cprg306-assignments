323d788191b565a93a2970378f2131f7b73a5fef
Shelyn Del Mundo <dm.shelyn@gmail.com>
2024-01-16 13:17:52 -0700

    week2 update

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,22 @@
-export default function page() {
+import Link from "next/link";
+
+import Heading from "./heading";
+
+export default function Page() {
+    let a = 5;
+    let b = 6;
+    let c = a + b;
     return (
         <main>
-            <h1 className="text-4xl">Week 2 Demos</h1>
+            <h1>CPRG 306 Demos</h1>
+            <Heading/>
+            <p>
+                <Link href = "week-2">Week 2</Link>
+            </p>
             <p>This is a paragraph</p>
             <p>This is another paragraph</p>
+            <p>The value of a is {a}.</p>
+            <p>The value of a and b is {a + b}</p>
         </main>
     
     );
