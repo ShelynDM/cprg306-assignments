16db477995ad13010e21e9c8fc8cb7823a100f34
Shelyn Del Mundo <dm.shelyn@gmail.com>
2024-01-16 13:00:34 -0700

    Create page.js

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+export default function page() {
+    return (
+        <main>
+            <h1 className="text-4xl">Week 2 Demos</h1>
+            <p>This is a paragraph</p>
+            <p>This is another paragraph</p>
+        </main>
+    
+    );
+  }
