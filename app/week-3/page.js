a37f4d3258057d923c9c6eaf07a46f9aec8c0c83
Shelyn Del Mundo <dm.shelyn@gmail.com>
2024-01-23 14:30:21 -0700

    week3 added

---
+++ app/week-3/page.js
@@ -0,0 +1,12 @@
+// import Link from "next/link";
+import ItemList from "./item-list";
+// import Item from "./item";
+
+export default function Page() {
+    return (
+      <main>
+        <h1 className="text-4x1">Shopping List</h1>
+        <ItemList/>
+        </main>
+    );
+  }
