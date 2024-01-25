d33c10586297b8352104dab7068983a3c3250934
Shelyn Del Mundo <dm.shelyn@gmail.com>
2024-01-25 10:41:15 -0700

    main page style update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
 import Link from 'next/link';
 export default function Page() {
   return (
-    <main className="m-3">
+    <main className="flex-auto flex-col items-center justify-between p-24">
       <div>
-        <h1 className='text-3xl'>CPRG 306: Web Development 2 - Assignments</h1>
-      </div>
-
-      <div className="text-2xl m-2">
-        <ul>
-          <li>
-            <Link href = "week-2">Week 2</Link>
-          </li>
-          <li>
-            <Link href = "week-3">Week 3</Link>
-          </li>
-        </ul>
+        <h1 className='text-3xl font-bold'>CPRG 306: Web Development 2 - Assignments</h1>
+        <div className="text-2xl m-2">
+          <ul className="text-lg">
+            <li className="hover:text-blue-400 hover:underline">
+              <Link href = "week-2">Week 2 Assignment</Link>
+            </li>
+            <li className="hover:text-blue-400 hover:underline">
+              <Link href = "week-3">Week 3 Assignment</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   );
